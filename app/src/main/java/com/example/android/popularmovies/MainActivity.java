c55176d7e98958b4b441c94fb5ee5b87f8f873e5
Soojeong <soojeongshin2@gmail.com>
2018-06-15 17:34:44 +0900

    feat: Check the network connection

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.android.popularmovies;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.popularmovies.MovieAdapter.MovieAdapterOnClickHandler;
@@ -32,6 +40,9 @@ public class MainActivity extends AppCompatActivity implements MovieAdapterOnCli
 
     /** Reference to RecyclerView */
     @BindView(R.id.rv_movie) RecyclerView mRecyclerView;
+
+    /** Reference to Offline TextView*/
+    @BindView(R.id.tv_offline) TextView mOfflineTextView;
 
     /** Reference to MovieAdapter*/
     private MovieAdapter mMovieAdapter;
@@ -63,7 +74,16 @@ public class MainActivity extends AppCompatActivity implements MovieAdapterOnCli
         mMovieAdapter = new MovieAdapter(movies, this);
         // Set the MovieAdapter to the RecyclerView
         mRecyclerView.setAdapter(mMovieAdapter);
-        // Once all of our views are setup, load the movie data
+
+        // Show a dialog when there is no internet connection
+        showNetworkDialog(isOnline());
+        // Once all of our views are set up, we can load the movie data.
+        loadMovieData();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         loadMovieData();
     }
 
@@ -72,6 +92,8 @@ public class MainActivity extends AppCompatActivity implements MovieAdapterOnCli
      * background method to get the movie data in the background.
      */
     private void loadMovieData() {
+        showMovieDataView();
+
         String sort = MoviePreferences.getPreferredSortCriteria(this);
         new FetchMovieTask().execute(sort);
     }
@@ -131,8 +153,76 @@ public class MainActivity extends AppCompatActivity implements MovieAdapterOnCli
 
             // Add the movie data
             if (movies != null && !movies.isEmpty()) {
+                showMovieDataView();
                 mMovieAdapter.addAll(movies);
             }
+            // If there is no internet connection, show the offline message
+            if (!isOnline()) {
+                showOfflineMessage();
+            }
         }
+    }
+
+    /**
+     * Check if there is the network connectivity
+     *
+     * @return true if connected to the network
+     */
+    private boolean isOnline() {
+        // Get a reference to the ConnectivityManager to check the state of network connectivity
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
+
+    /**
+     * Show a dialog when there is no internet connection
+     *
+     * @param isOnline true if connected to the network
+     */
+    private void showNetworkDialog(final boolean isOnline) {
+        if (!isOnline) {
+            // Create an AlertDialog.Builder
+            AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.Theme_AppCompat_Dialog_Alert);
+            // Set an Icon and title, and message
+            builder.setIcon(R.drawable.ic_warning);
+            builder.setTitle(getString(R.string.no_network_title));
+            builder.setMessage(getString(R.string.no_network_message));
+            builder.setPositiveButton(getString(R.string.go_to_settings), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivityForResult(new Intent(Settings.ACTION_SETTINGS),0);
+                }
+            });
+            builder.setNegativeButton(getString(R.string.cancel), null);
+
+            // Create and show the AlertDialog
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+    }
+
+    /**
+     * This method will make the View for the movie data visible and
+     * hide the offline message.
+     */
+    private void showMovieDataView() {
+        // First, make sure the offline message is invisible
+        mOfflineTextView.setVisibility(View.INVISIBLE);
+        // Then, make sure the movie data is visible
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * This method will make the offline message visible and hide the movie View
+     */
+    private void showOfflineMessage() {
+        // First, hide the currently visible data
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        // Then, show the offline message
+        mOfflineTextView.setVisibility(View.VISIBLE);
     }
 }
