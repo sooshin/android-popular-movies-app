3d6e82a9f2853eb3e4fca03d8a14a9b0c60203f6
Soojeong <soojeongshin2@gmail.com>
2018-07-06 01:11:35 +0900

    feat: Create a Data Access Object

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDao.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2018 Soojeong Shin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example.android.popularmovies.data;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * {@link Dao} which provides an API for all data operations with the MovieDatabase.
+ */
+@Dao
+public interface MovieDao {
+
+    @Query("SELECT * FROM movie")
+    LiveData<List<MovieEntry>> loadAllMovies();
+
+    @Insert
+    void insertMovie(MovieEntry movieEntry);
+
+    @Delete
+    void deleteMovie(MovieEntry movieEntry);
+
+    @Query("SELECT * FROM movie WHERE movie_id = :movieId")
+    LiveData<MovieEntry> loadMovieByMovieId(int movieId);
+}
