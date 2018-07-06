59e053c3e14b7f991998c89b42f93fe51089215d
Soojeong <soojeongshin2@gmail.com>
2018-07-07 04:00:05 +0900

    feat: Create InjectorUtils

---
+++ app/src/main/java/com/example/android/popularmovies/utilities/InjectorUtils.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 2018 Soojeong Shin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example.android.popularmovies.utilities;
+
+import android.content.Context;
+
+import com.example.android.popularmovies.AppExecutors;
+import com.example.android.popularmovies.data.MovieDatabase;
+import com.example.android.popularmovies.data.MovieRepository;
+import com.example.android.popularmovies.viewmodel.MainViewModelFactory;
+import com.example.android.popularmovies.viewmodel.ReviewViewModelFactory;
+import com.example.android.popularmovies.viewmodel.TrailerViewModelFactory;
+
+/**
+ * Provides static methods to inject the various classes needed for PopularMovies
+ */
+public class InjectorUtils {
+
+    public static MovieRepository provideRepository(Context context) {
+        MovieDatabase database = MovieDatabase.getInstance(context.getApplicationContext());
+        AppExecutors executors = AppExecutors.getInstance();
+        // The Retrofit class generates an implementation of the TheMovieApi interface
+        TheMovieApi theMovieApi = Controller.getClient().create(TheMovieApi.class);
+        return MovieRepository.getInstance(database.movieDao(), theMovieApi, executors);
+    }
+
+    public static MainViewModelFactory provideMainActivityViewModelFactory(Context context, String sortCriteria) {
+        MovieRepository repository = provideRepository(context.getApplicationContext());
+        return new MainViewModelFactory(repository, sortCriteria);
+    }
+
+    public static ReviewViewModelFactory provideReviewViewModelFactory(Context context, int movieId) {
+        MovieRepository repository = provideRepository(context.getApplicationContext());
+        return new ReviewViewModelFactory(repository, movieId);
+    }
+
+    public static TrailerViewModelFactory provideTrailerViewModelFactory(Context context, int movieId) {
+        MovieRepository repository = provideRepository(context.getApplicationContext());
+        return new TrailerViewModelFactory(repository, movieId);
+    }
+}
