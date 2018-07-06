baf8828dfdc4cfc90b9683d78ea2e5214ca08eef
Soojeong <soojeongshin2@gmail.com>
2018-07-06 23:49:35 +0900

    feat: Create the MainActivity ViewModel and Main ViewModelFactory

---
+++ app/src/main/java/com/example/android/popularmovies/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2018 Soojeong Shin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example.android.popularmovies.viewmodel;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.android.popularmovies.data.MovieRepository;
+import com.example.android.popularmovies.model.MovieResponse;
+
+/**
+ * {@link ViewModel} for MainActivity
+ */
+public class MainActivityViewModel extends ViewModel {
+
+    private final MovieRepository mRepository;
+    private final LiveData<MovieResponse> mMovieResponse;
+
+    public MainActivityViewModel(MovieRepository repository, String sortCriteria) {
+        mRepository = repository;
+        mMovieResponse = mRepository.getMovies(sortCriteria);
+    }
+
+    public LiveData<MovieResponse> getMovieResponse() {
+        return mMovieResponse;
+    }
+}
