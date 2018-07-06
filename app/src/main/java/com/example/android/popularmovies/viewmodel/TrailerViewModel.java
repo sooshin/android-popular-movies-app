33a7c593ba9c83e4a1eceb1a2886f209941c1fd9
Soojeong <soojeongshin2@gmail.com>
2018-07-07 00:59:44 +0900

    feat: Create the TrailerFragment ViewModel

---
+++ app/src/main/java/com/example/android/popularmovies/viewmodel/TrailerViewModel.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2018 Soojeong Shin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example.android.popularmovies.viewmodel;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.android.popularmovies.data.MovieRepository;
+import com.example.android.popularmovies.model.VideoResponse;
+
+/**
+ * {@link ViewModel} for TrailerFragment
+ */
+public class TrailerViewModel extends ViewModel {
+
+    private final MovieRepository mRepository;
+    private final LiveData<VideoResponse> mVideoResponse;
+
+    public TrailerViewModel (MovieRepository repository, int movieId) {
+        mRepository = repository;
+        mVideoResponse = mRepository.getVideoResponse(movieId);
+    }
+
+    public LiveData<VideoResponse> getVideoResponse() {
+        return mVideoResponse;
+    }
+}
---
+++ app/src/main/java/com/example/android/popularmovies/viewmodel/TrailerViewModelFactory.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2018 Soojeong Shin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example.android.popularmovies.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.example.android.popularmovies.data.MovieRepository;
+
+/**
+ * Factory method that allows us to create a ViewModel with a constructor that takes a
+ * {@link MovieRepository} and the movie ID
+ */
+public class TrailerViewModelFactory extends ViewModelProvider.NewInstanceFactory{
+
+    private final MovieRepository mRepository;
+    private final int mMovieId;
+
+    public TrailerViewModelFactory (MovieRepository repository, int movieId) {
+        this.mRepository = repository;
+        this.mMovieId = movieId;
+    }
+
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass) {
+        //noinspection unchecked
+        return (T) new TrailerViewModel(mRepository, mMovieId);
+    }
+}
